3a5cc1053284e7cf951c5d0419b7e74c9f4348e9
Valentin Ruiz Lopez <varulo.cvcv@gmail.com>
2021-01-29 22:50:01 +0100

    Header component

---
+++ components/Header.js
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+
+export const Header = (props) => {
+
+  const { width, height, href } = props;
+  const router = useRouter();
+
+  return (
+    <div className='py-4 px-12'>
+      <Image src='/img/FlattenedLogo.svg' alt="coders-agora-image-logo" width={width} height={height} />
+      
+      { router.pathname === href ?
+        <h1 className="inline">{router.pathname.slice(1).split('/')}</h1>
+        :
+        <Image src='/img/Coders’ Agora.svg' alt="coders-agora-text-logo" width={500} height={height} />
+      }
+      
+    </div>
+  )
+}
--- components/Hero.js
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-
-export const Hero = () => {
-  return (
-    <div className='py-4 px-12'>
-      <Image src='/img/TextLogo.svg' width={639} height={100} />
-    </div>
-  )
-}
