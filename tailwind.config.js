61d17444cdf223df2ac921c3cf1aa61c271f39a0
ArayoAlejandro <alejandroarayo426@gmail.com>
2021-01-31 13:05:54 +0100

    Purge empty

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: ['./pages/**/*.js', './components/**/*.js'],
+// purge: ['./pages/**/*.js', './components/**/*.js'],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
