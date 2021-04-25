ad5283b195e89d97fa319d819f2348a0f14dbf58
J. Saura <jose.saura@estudiocactus.com>
2021-04-25 13:15:45 +0200

    Lil changes

--- components/TextPanel.js
+++ components/TextPanel.js
@@ -3,7 +3,7 @@ export const TextPanel = ({ title, body, full }) => {
     <>
       <div className={`text-panel relative pb-6 pt-1 ${full ? 'w-full' : 'w-full lg:w-1/2'}`}>
         <h3 className='text-panel__title z-10 mt-0 mb-4 text-ca-text-black'>{title}</h3>
-        <p className='text-panel__body z-10'>Una comunidad de progamador_s donde compartir conocimientos, experiencias y consejos. Con la <strong>principal misión</strong> de mejorar la experiencia de aprendizaje y conocer mejor la realidad del mundo laboral.`</p>
+        <p className='text-panel__body z-10'>Una joven comunidad de gente interesada en la programación, desarrollo y diseño de productos digitales, donde compartir conocimientos, experiencias y consejos. Con la <strong>principal misión</strong> de mejorar la experiencia de aprendizaje y conocer mejor la realidad del mundo laboral.`</p>
         <div className='text-panel__background bg-ca-agora-pink-10 h-full w-3/5 absolute top-0 -left-10 z-0 ' />
       </div>
       <style jsx>{`
